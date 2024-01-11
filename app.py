182aafb50d38f425d019fcd9daa4c8987cbaea91
BricoBC <barajas.c.bruno.n@gmail.com>
2024-01-10 23:22:26 -0600

    Change requirements and add config of page to project

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.set_page_config(page_title="Brico's Analytics", page_icon="📊", layout="wide")
+st.title('Analisis desde la web')
