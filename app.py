a9895e9112e2a02135931392880863193dfd2b1a
BricoBC <barajas.c.bruno.n@gmail.com>
2024-01-13 23:41:42 -0600

    Add function to view data

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ import numpy as np
 
 
 st.set_page_config(page_title="Brico's Analytics", page_icon="📊", layout="wide")
-st.title('Analisis desde la web')
-
-# Add a slider to the sidebar:
-data = st.sidebar.file_uploader('Carga el archivo para empezar: ')
-
-if data is not None:
+st.title('Análisis desde la web')
+    
+def showCharts(file):
     df = pd.read_csv(data)
     st.write(df)
+
+# Add a slider to the sidebar:
+data = st.sidebar.file_uploader('Carga el archivo para empezar ')
+
+if data is not None:
+    showCharts(data)
