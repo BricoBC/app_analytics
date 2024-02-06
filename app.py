62e93dc4df8fa7a4bbfdd037a277b48799a1caf8
BricoBC <barajas.c.bruno.n@gmail.com>
2024-02-06 17:04:30 -0600

    Modify Uploader of file ready work

--- app.py
+++ app.py
@@ -3,23 +3,36 @@ import pandas as pd
 import numpy as np
 import datetime
 
-data = None
 st.set_page_config(page_title="Brico's Analytics", page_icon="📊", layout="wide")
     
-def showCharts(file):
+def showCharts(file):    
     st.title('Análisis de la Tienda')
-    df = pd.read_excel(data)
+    
     
 
 def sidebar():
+    data = None
     with st.sidebar:
         type_file = ['csv', 'excel']
-        file = st.radio('Tipo de archivo: ', type_file)
+        file = st.radio('Tipo de archivo: ', type_file, index=None)
         
         if file == 'excel':
             data = st.file_uploader('Carga el archivo para empezar ', type=["xlsx"])        
+            st.write(data)
+            
+            if data is not None:
+                showCharts(data)
+                
         if file == 'csv' :            
-            data = st.file_uploader('Carga el archivo para empezar ', type=["csv"], accept_multiple_files=True)        
+            data = st.file_uploader('Carga el archivo para empezar ', type=["csv"], accept_multiple_files=True)  
+            st.write(data)
+            
+            if len(data) > 0:
+                showCharts(data)
+                
+                
+            
+        
                         
 
 
@@ -28,10 +41,6 @@ def sidebar():
 #                              , max_value= datetime.date(2021, 12, 31))
 # year_end = st.sidebar.date_input("Selecciona la fecha de final", value = datetime.date(2019, 7, 6), min_value=datetime.date(2019, 1, 1)
 #                              , max_value= datetime.date(2021, 12, 31))
-
-
-if data is not None:
-    showCharts(data)
     
-if __name__ == '__main__':
+if __name__ == '__main__':    
     sidebar()
